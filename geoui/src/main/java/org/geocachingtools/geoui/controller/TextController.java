8542fa19479bfe2a9ae69edb43ffb0e7fba623bc
Jakob <20120451@IFTS4.htl-stp.if>
2017-01-23 12:57:54 +0100

    Added Tooltips Added class textController.java

---
+++ geoui/src/main/java/org/geocachingtools/geoui/controller/TextController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.geocachingtools.geoui.controller;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author 20120451
+ */
+@Named(value = "textController")
+@SessionScoped
+public class TextController implements Serializable {
+
+    /**
+     * Creates a new instance of TextController
+     */
+    public TextController() {
+    }
+    
+    
+}
