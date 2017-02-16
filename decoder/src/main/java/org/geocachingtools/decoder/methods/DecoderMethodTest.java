205dbb6f43ee4642da9672f77e1724c73429e7e0
KURZ <s.kurzbauer@htlstp.at>
2017-02-16 18:01:54 +0100

    download dictionary

--- decoder/src/main/java/org/geocachingtools/decoder/methods/DecoderMethodTest.java
+++ decoder/src/main/java/org/geocachingtools/decoder/methods/DecoderMethodTest.java
@@ -1,24 +1,27 @@
 package org.geocachingtools.decoder.methods;
 
 import org.geocachingtools.decoder.*;
+import org.geocachingtools.validator.Validator;
+import org.geocachingtools.validator.ValidatorRequest;
 
 /**
  *
  * @author Simon
  */
-@Method(name = "Test Method", requiresPassword = false, type=String.class)
+@Method(name = "Test Validator", requiresPassword = false, type=String.class)
 public class DecoderMethodTest extends DecoderMethod<String>{
     I18n i18n;
     
     @Override
     public DecoderResult decode(DecoderRequest<String> request) {
         i18n = new I18n(request.getLocale());
-        
+        double rating = Validator.getInstance().check(new ValidatorRequest(request.getData())).getRelevance();
+        String result = String.format("%.2f "+request.getData(), rating);
         return new DecoderResult(
                 this,
-                i18n.get("TEST"),
-                "Das ist ein Testilein: " + request.getData().replace("<", "").replace("\"", "").replace("'", ""),
-                1.0
+                result,
+                "Full:" +result,
+                rating
         );
     }
     
